431162d84432faf5a10216f935b58ef1c0f0fcfc
Carlos Fajardo <ce_fajardo@hotmail.com>
2020-04-09 23:07:50 -0500

    Initial commit

---
+++ src/main/java/com/cefajardo/springbootkeycloak/Application.java
@@ -0,0 +1,14 @@
+package com.cefajardo.springbootkeycloak;
+
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
